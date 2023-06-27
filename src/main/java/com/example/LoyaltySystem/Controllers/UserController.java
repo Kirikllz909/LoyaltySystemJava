3bdcb3b498e2ea257e8de76e629da604731a690e
Kirikllz909 <kirikllz909@gmail.com>
2023-06-27 23:41:13 +0300

    Removing unnecessary imports

--- src/main/java/com/example/LoyaltySystem/Controllers/UserController.java
+++ src/main/java/com/example/LoyaltySystem/Controllers/UserController.java
@@ -3,16 +3,12 @@ package com.example.LoyaltySystem.Controllers;
 import com.example.LoyaltySystem.Models.User;
 import com.example.LoyaltySystem.Repositories.UserRepository;
 
-import java.util.Optional;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 
