6aaa929a8bb738f47069e66bf0de9c53ed50e1da
Kirikllz909 <kirikllz909@gmail.com>
2023-06-27 23:40:00 +0300

    Fix problems with work of crud operations

--- src/main/java/com/example/LoyaltySystem/Controllers/UserController.java
+++ src/main/java/com/example/LoyaltySystem/Controllers/UserController.java
@@ -1,39 +1,65 @@
 package com.example.LoyaltySystem.Controllers;
 
 import com.example.LoyaltySystem.Models.User;
+import com.example.LoyaltySystem.Repositories.UserRepository;
 
+import java.util.Optional;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/user")
-    public String getUser(@RequestParam(value = "id", defaultValue = "1") int id) {
-        return "It' working";
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Integer id)
+            throws Exception {
+        return userRepository.findById(id).orElseThrow(() -> new Exception("User not found"));
+
     }
 
-    @PutMapping("/addUser")
+    @GetMapping("/users")
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+
     @PostMapping("/addUser")
-    public void addUser(@RequestBody User user) {
+    public String addUser(@RequestBody User user) throws Exception {
+        try {
+            user.setEmail(user.getEmail().toLowerCase());
+            userRepository.save(user);
+        } catch (Exception e) {
+            return "Unable to add user. " + user.getEmail() + " is already used";
+        }
+        return "User was successfully added";
     }
 
-    @DeleteMapping("/deleteUser")
-    @PostMapping("/deleteUser")
-    public void deleteUser(@RequestParam(value = "id") int id) {
-
+    @DeleteMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+        return "User was successfully deleted";
     }
 
-    @PatchMapping("/patchUser")
-    @PostMapping("/patchUser")
-    public void patchUser(@RequestParam(value = "id") int id) {
-
+    @PutMapping("/updateUser/{id}")
+    public User patchUser(@PathVariable Integer id, @RequestBody User newUser) throws Exception {
+        return userRepository.findById(id).map(user -> {
+            user.setFirstName(newUser.getFirstName());
+            user.setLastName(newUser.getLastName());
+            user.setEmail(newUser.getEmail());
+            user.setPhone(newUser.getPhone());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new Exception("User not found"));
     }
 
 }
