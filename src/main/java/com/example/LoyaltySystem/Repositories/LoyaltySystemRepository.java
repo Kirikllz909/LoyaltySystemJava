fa82093b57898fdb531eb55f3c34eac61fa3dbb0
Kirikllz909 <kirikllz909@gmail.com>
2023-06-28 15:47:51 +0300

    Add repo for crud requests for loyalty system

---
+++ src/main/java/com/example/LoyaltySystem/Repositories/LoyaltySystemRepository.java
@@ -0,0 +1,8 @@
+package com.example.LoyaltySystem.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.LoyaltySystem.Models.LoyaltySystem;
+
+public interface LoyaltySystemRepository extends CrudRepository<LoyaltySystem, Integer> {
+
+}
