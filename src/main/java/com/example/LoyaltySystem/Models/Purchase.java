e2dfd49373f1fa66c786667b0c514fc09f86a5a9
Kirikllz909 <kirikllz909@gmail.com>
2023-06-29 18:19:31 +0300

    Add missing getters and changed throwing exception

--- src/main/java/com/example/LoyaltySystem/Models/Purchase.java
+++ src/main/java/com/example/LoyaltySystem/Models/Purchase.java
@@ -2,10 +2,16 @@ package com.example.LoyaltySystem.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import com.example.LoyaltySystem.Exceptions.NegativeValueException;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 
 @Entity
@@ -13,6 +19,7 @@ import jakarta.persistence.Column;
 public class Purchase {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column(name = "purchaseSum")
@@ -20,16 +27,28 @@ public class Purchase {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
-    public void setPurchaseSum(double purchaseSum) throws Exception {
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setPurchaseSum(double purchaseSum) throws NegativeValueException {
         if (purchaseSum < 0)
-            throw new Exception("Sum of purchase should be positive");
+            throw new NegativeValueException("Purchase sum");
         this.purchaseSum = purchaseSum;
     }
 
-    public double purchaseSum() {
+    public double getPurchaseSum() {
         return this.purchaseSum;
     }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
 }
